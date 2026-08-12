21b3fccc1ee78df0ae3d6df72b1b3d134a595581
joe-takainter <itakeshi00@gmail.com>
2026-08-12 03:08:37 +0000

    Add about page

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "こんにちは Flask！"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return "これは住所録アプリの説明ページです。"
 
 
 if __name__ == "__main__":
