f2ade63066df0c91f2681f4a48ba0db5679771e8
joe-takainter <itakeshi00@gmail.com>
2026-08-13 14:37:49 +0000

    Add people list page

--- app-old31.py
+++ app-old31.py
@@ -3,13 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-people = [
-    {"name": "Takeshi", "age": "30", "hobby": "ボウリング"},
-    {"name": "Yamada", "age": "25", "hobby": "野球"},
-    {"name": "Suzuki", "age": "40", "hobby": "ゴルフ"}
-]
-
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -18,11 +11,6 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-
-@app.route("/people")
-def show_people():
-    return render_template("people.html", people=people)
 
 
 if __name__ == "__main__":
